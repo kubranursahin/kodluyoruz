ebefa669cfbf552d4f5f20ae8effe046142904f5
Kübranur Şahin <78857206+kubranursahin@users.noreply.github.com>
2022-02-06 01:34:26 +0300

    Update vkiHesabi.java

--- vkiHesabi.java
+++ vkiHesabi.java
@@ -1,17 +1,7 @@
 import java.util.Scanner;
 public class vkiHesabi
 {
-   /* Java ile kullanıcıdan boy ve kilo değerlerini
-    alıp bir değişkene atayın. Aşağıdaki formüle
-    göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
-
-    Formül
-    Kilo (kg) / Boy(m) * Boy(m)
-    Çıktısı
-    Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
-    Lütfen kilonuzu giriniz : 105
-    Vücut Kitle İndeksiniz : 35.49215792320173
-    */
+ 
     public static void main(String[] args)
 {
     double boy, kilo, vki;
